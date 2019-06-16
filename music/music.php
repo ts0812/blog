7ea025dc4f161cf3ef5eda4e6861b812dfce8b44
ts <1352645017@qq.com>
2019-06-16 18:49:54 +0800

    1

--- music/music.php
+++ music/music.php
@@ -6,21 +6,21 @@
         $switch = $_GET['switch']??0;
         if($id){
             if($switch==0){
-                $sql = 'select *from music where state=1 and id<'.$id . ' order by id desc limit 0,1 ';
+                $sql = 'select *from music where state>0 and playlist=1 and id<'.$id . ' order by id desc limit 0,1 ';
                 if($res = find($sql)){
                     die(json_encode(['type'=>'1','message'=>'ok','data'=>$res['id']]));
                 }
-                $sql = 'select *from music where state=1 order by id desc limit 0,1 ';
+                $sql = 'select *from music where  state>0 and playlist=1 order by id desc limit 0,1 ';
                 if($res = find($sql)){
                     die(json_encode(['type'=>'1','message'=>'ok','data'=>$res['id']]));
                 }
                 die(json_encode(['type'=>'0','message'=>'没有了']));
             }else{
-                $sql = 'select *from music where state=1 and id>'.$id . ' limit 0,1 ';
+                $sql = 'select *from music where state>0 and playlist=1 and id>'.$id . ' limit 0,1 ';
                 if($res = find($sql)){
                     die(json_encode(['type'=>'1','message'=>'ok','data'=>$res['id']]));
                 }
-                $sql = 'select *from music where state=1 limit 0,1 ';
+                $sql = 'select *from music where state>0 and playlist=1 limit 0,1 ';
                 if($res = find($sql)){
                     die(json_encode(['type'=>'1','message'=>'ok','data'=>$res['id']]));
                 }
